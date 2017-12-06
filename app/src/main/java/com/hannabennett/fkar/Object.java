b84923008132b1a0b82dde7a2e478263894064fd
Hanna Bennett <bennetthanna22@gmail.com>
2017-12-06 09:55:07 -0800

    Create model

---
+++ app/src/main/java/com/hannabennett/fkar/Object.java
@@ -0,0 +1,36 @@
+package com.hannabennett.fkar;
+
+/**
+ * Created by HannaBennett on 12/6/17.
+ */
+
+public class Object {
+    private int mXCoordinate;
+    private int mYCoordinate;
+    private int mSpeed;
+
+    public int getXCoordinate() {
+        return mXCoordinate;
+    }
+
+    public void setXCoordinate(int xCoordinate) {
+        mXCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return mYCoordinate;
+    }
+
+    public void setYCoordinate(int yCoordinate) {
+        mYCoordinate = yCoordinate;
+    }
+
+    public int getSpeed() {
+        return mSpeed;
+    }
+
+    public void setSpeed(int speed) {
+        mSpeed = speed;
+    }
+
+}
